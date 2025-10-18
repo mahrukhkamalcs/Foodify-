5c8c85343c0cad5c22baf9d20952b0220cee5794
mahrukhkamalcs <kamalmahrukh00@gmail.com>
2025-10-18 12:33:32 +0100

    Add home screen feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:flutter/services.dart';
 import 'core/theme/app_theme.dart';
 import 'navigation/app_router.dart';
 import 'features/onboarding/onboarding_screen.dart';
-=======
 import 'orderstatus.dart';
 import 'profile.dart';
->>>>>>> main
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,22 +25,11 @@ class FoodifyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
       title: 'Foodify',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-     
-      onGenerateRoute: AppRouter.generateRoute, //  Centralized navigation
-       initialRoute: OnboardingScreen.routeName,
-=======
-      debugShowCheckedModeBanner: false,
-      title: 'Food Delivery App',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.orange,
-      ),
-      home: const OrderStatusScreen(),
->>>>>>> main
+      onGenerateRoute: AppRouter.generateRoute,
+      initialRoute: OnboardingScreen.routeName,
     );
   }
 }
